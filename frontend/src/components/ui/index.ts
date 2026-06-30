4b519a25cafc423e1130b5a48a6884011ccaf618
Harsh-Vardhan-Singh29 <2k24.cs1i.2412697@gmail.com>
2026-06-30 12:06:27 +0530

    fix: remove stale AnimatedCard export

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as GlassCard } from "./GlassCard";
-export { default as AnimatedCard } from "./AnimatedCard";
 export { default as AnimatedCounter } from "./AnimatedCounter";
 export { default as SectionHeader } from "./SectionHeader";
 export { default as ProgressRing } from "./ProgressRing";
